3ffbb62a9c5472616a5726b807b29f9ffda66a2f
TADtech-mayor <tchatodaniel13@gmail.com>
2026-02-21 17:51:57 +0000

    j'ai finalement cacher mon API gemini

--- gemini.js
+++ gemini.js
@@ -1,10 +1,10 @@
 // c'est pour pouvoir utiliser mon api stocker dans le fichier .env 
-// require('dotenv').config()
+require('dotenv').config()
 const { GoogleGenerativeAI } = require('@google/generative-ai')
 
 // initialisation de gemini avec ma clée 
-// const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const genAI = new GoogleGenerativeAI('AIzaSyBTGUL_98eC9q1p_d8n9TqPQR34_g2JsVk');
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+// const genAI = new GoogleGenerativeAI('');
 // le choix de model de Gemini 
 const model = genAI.getGenerativeModel({ model:'gemini-2.5-flash'})
 
